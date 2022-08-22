f6e4702bf14a20db1edb43c175a9fea9c51a9799
Devil5614737 <kaushiksheel9@gmail.com>
2022-08-22 22:12:29 +0530

    adding dice feature

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const apiLink="https://api.quotable.io/random?tags=technology,famous-quotes";
 const quote_container=document.querySelector('.content');
+const dice=document.querySelector('.diceIcon')
 
 async function fetchData(){
 const res=await fetch(apiLink);
@@ -7,5 +8,9 @@ const data=await res.json();
 quote_container.textContent=`"${data.content}"`
 console.log(data.content)
 }
-
 fetchData()
+dice.addEventListener('click',()=>{
+
+    fetchData()
+})
+
